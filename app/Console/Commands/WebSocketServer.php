f1a3fcc872c180303a6581e11afc668e3bc88886
sahilkashyap64 <sahilkashyap64@gmail.com>
2021-10-24 18:16:00 +0530

    for wss in prodution

--- app/Console/Commands/WebSocketServer.php
+++ app/Console/Commands/WebSocketServer.php
@@ -7,6 +7,9 @@ use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use App\Http\Controllers\QRLoginWS\QRLoginWSController as WebSocketController;
+use React\EventLoop\Factory;
+use React\Socket\SecureServer;
+use React\Socket\Server;
 
 class WebSocketServer extends Command
 {
@@ -41,6 +44,13 @@ class WebSocketServer extends Command
      */
     public function handle()
     {
+        $this->forlocal();
+        // $this->forprodserver();
+    }
+    
+    
+    public function forlocal()
+    {
         $server = IoServer::factory(
             new HttpServer(
                 new WsServer(
@@ -50,5 +60,19 @@ class WebSocketServer extends Command
             8090
         );
         $server->run();
+    }
+
+    public function forprodserver()
+    {
+        $loop = Factory::create();
+        $webSock = new SecureServer(new Server('0.0.0.0:8090', $loop) , $loop, array(
+            'local_cert' => '/etc/letsencrypt/live/test.tv.com/fullchain.pem', // path to your cert
+            'local_pk' => '/etc/letsencrypt/live/test.tv.com/privkey.pem', // path to your server private key
+            'allow_self_signed' => true, // Allow self signed certs (should be false in production)
+            'verify_peer' => false
+        ));
+        // Ratchet magic
+        $webServer = new IoServer(new HttpServer(new WsServer(new WebSocketController())) , $webSock);
+        $loop->run();
     }
 }
